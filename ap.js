1ba1321ffccdd0e38c5ead7ecc19f627d3b15567
MatLisel <166405961+MatLisel@users.noreply.github.com>
2024-04-22 17:23:53 +0200

    zad1

---
+++ ap.js
@@ -0,0 +1,28 @@
+const carsJS = require('./cars');
+const htmlJS = require('./htmlgenerator');
+
+const http = require('http');
+
+const carsJS = require('./cars');
+const htmlJS = require('./htmlgenerator');
+
+const PORT = 3000;
+
+const requestListener = (req, res) => {
+    const cars = carsJS.getCars();
+    console.log(cars);
+    res.setHeader("Content-Type", "text/html");
+    res.write(htmlJS.getHTMLDocumentStart());
+    res.write('<body>');
+    res.write(`<p>${carsJS.getCarInformation(1)}</p>`);
+    res.write(`<p>${carsJS.getCarAge(1)}</p>`);
+    res.write('</body>');
+    res.write(htmlJS.getHTMLDocumentEnd());
+    res.end();
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`);
+});
